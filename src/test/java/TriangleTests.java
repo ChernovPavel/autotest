4f824b7385bfe965b026db33c01636c837e5fb80
Pavel Chernov <chernov-pa@yandex-team.ru>
2019-04-22 16:42:59 +0300

    fixes test

--- src/test/java/TriangleTests.java
+++ src/test/java/TriangleTests.java
@@ -9,35 +9,39 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class TriangleTests {
+
+    @Parameterized.Parameter()
     public int a;
+
+    @Parameterized.Parameter(1)
     public int b;
+
+    @Parameterized.Parameter(2)
     public int c;
+
+    @Parameterized.Parameter(3)
     public boolean expected;
 
-    public TriangleTests(int a, int b, int c, boolean expected) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.expected = expected;
-    }
-
-    @Parameterized.Parameters
+    @Parameterized.Parameters (name = "a= {0} b={1} c={2} : result - {3}")
     public static Collection<Object[]> parameters() {
         return Arrays.asList(new Object[][]{
                 {5, 4, 3, true},
+                {0, 10, 1, false},
+                {1, 0, 9, false},
+                {7, 7, 0, false},
                 {2, 9, 8, true},
                 {1, 1, 5, false},
-                {14, 15, 28, true},
-                {1, 5, 12, false},
+                {-14, -15, -28, false},
                 {0, 0, 0, false},
                 {1, -5, 12, false}
         });
     }
 
     @Test
-    public void IsTriangleShouldTriangleExists() {
-        Triangle t = new Triangle();
-        boolean actual = t.isTriangle(a, b, c);
+    public void isTriangleShouldTriangleExists() {
+        Triangle triangle = new Triangle(a, b, c);
+        //проверка существования треугольника с заданными сторонами
+        boolean actual = triangle.isTriangle();
         assertEquals(expected, actual);
     }
 
